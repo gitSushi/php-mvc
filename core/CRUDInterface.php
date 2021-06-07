dea14e545858195cfb24f95da855151ccbc2085c
gitSushi <lossushi37@gmail.com>
2021-06-07 14:21:13 +0000

    documented interfaces crud and repo

--- core/CRUDInterface.php
+++ core/CRUDInterface.php
@@ -2,8 +2,27 @@
 
 interface CRUDInterface
 {
+    /**
+     * @param int $id : prend en argument l’id de l’entité à manipuler
+     * @return object  retourne une entité
+     */
     public function retrieve($id);
+
+    /**
+     * @param int $id : prend en argument l’id de l’entité à manipuler
+     * @return bool un booléen de la réussite du traitement
+     */
     public function update($id);
+
+    /**
+     * @param int $id : prend en argument l’id de l’entité à manipuler
+     * @return bool un booléen de la réussite du traitement 
+     */
     public function delete($id);
+
+    /**
+     * @param int $associativeArray : prend en argument un tableau associatif des propriétés
+     * @return object  retourne une entité
+     */
     public function create($associativeArray);
 }
--- core/RepositoryInterface.php
+++ core/RepositoryInterface.php
@@ -2,6 +2,15 @@
 
 interface RepositoryInterface
 {
+    /**
+     * @return array les données
+     */
     public function getAll();
+
+    /**
+     * @param array $associativeArray : prend en argument un tableau associatif
+     *              pour les clauses WHERE et AND des requêtes SQL
+     * @return array les données
+     */
     public function getAllBy($associativeArray);
 }
