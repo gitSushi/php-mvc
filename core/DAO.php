f3e0b0109356a6e082ecde6b7fdeee1cec0d5227
gitSushi <lossushi37@gmail.com>
2021-06-07 19:00:25 +0000

    prise en charge du fichier config/database.json

--- core/DAO.php
+++ core/DAO.php
@@ -9,7 +9,15 @@ abstract class DAO implements RepositoryInterface, CRUDInterface
 
     public function __construct()
     {
-        $this->pdo = new \PDO("dsn");
+        $config = json_decode(file_get_contents("config/database.json"));
+        $dsn = "mysql:host=$config->host;dbname=$config->dbname;charset=utf8;port=$config->port";
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $this->pdo = new \PDO($dsn, $config->username, $config->password, $options);
     }
 
 
@@ -36,7 +44,6 @@ abstract class DAO implements RepositoryInterface, CRUDInterface
 
     public function getAll()
     {
-        return $this;
     }
 
     public function getAllBy($associativeArray)
