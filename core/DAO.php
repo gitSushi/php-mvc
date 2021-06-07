26bf55ffff3b06028bb9c36fa602bc9e8fc01e8c
gitSushi <lossushi37@gmail.com>
2021-06-07 17:48:01 +0000

    had forgotten the include

--- core/DAO.php
+++ core/DAO.php
@@ -1,13 +1,17 @@
 <?php
 
+include_once('core/RepositoryInterface.php');
+include_once('core/CRUDInterface.php');
+
 abstract class DAO implements RepositoryInterface, CRUDInterface
 {
     protected $pdo;
 
     public function __construct()
     {
-        $this->pdo = new PDO('dsn');
+        $this->pdo = new \PDO("dsn");
     }
+
 
     public function retrieve($id)
     {
@@ -32,6 +36,7 @@ abstract class DAO implements RepositoryInterface, CRUDInterface
 
     public function getAll()
     {
+        return $this;
     }
 
     public function getAllBy($associativeArray)
