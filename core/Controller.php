28ae91df77c9f8429a2a395caf26845fbde074a7
gitSushi <lossushi37@gmail.com>
2021-06-10 10:32:05 +0000

    prise en charge des inputs post et get

--- core/Controller.php
+++ core/Controller.php
@@ -2,15 +2,44 @@
 
 abstract class Controller
 {
+    /**
+     * Déclaration des variables privées (seulement accessible par les méthodes) get et post
+     */
     private $get, $post;
 
+    /**
+     * affectation de super globales $_GET et $_POST aux propriétés get et post
+     */
     public function __construct()
     {
         $this->get = $_GET;
         $this->post = $_POST;
     }
 
-    public function render($pathToView, $data)
+    /**
+     * la fonction render
+     * @param string $pathToView
+     * @param array $data
+     */
+    protected function render(string $pathToView, array $data)
     {
+    }
+
+    /**
+     * final empêche la réécriture
+     * retourne la propriété get contenant la super globale $_GET
+     */
+    protected final function inputGet()
+    {
+        return $this->get;
+    }
+
+    /**
+     * final empêche la réécriture
+     * retourne la propriété post contenant la super globale $_POST
+     */
+    protected final function inputPost()
+    {
+        return $this->post;
     }
 }
